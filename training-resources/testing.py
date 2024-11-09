f33e870518888fa095e604fe63f9c96ab2901ed6
Justin <justinch@uw.edu>
2024-11-08 17:08:28 -0800

    adjusted  testing to testing.py

--- training-resources/testing.py
+++ training-resources/testing.py
@@ -2,6 +2,24 @@
 
 from client import *
 
-data = make_query("Running Total")
+# Example: Call client.py to run OR import module and call make query
+test = make_query("Excellent cardio exercises for short on time")
 
-print(data)
+# Return value of function will be lists of 10 dictionaries marking top 10 hits from a google search with search phrase
+# 3 fields in dictionary: URL, Title, and Description
+for entry in test: 
+    print("")
+    print(entry['url'])
+    print(entry['title'])
+    print(entry['description'])
+    print("")
+
+# To get a specific entry (i.e, top search result),
+print(test[0])
+
+# And for a specific information for an entry
+
+print(test[0]['url'])
+print(test[0]['title'])
+print(test[0]['description'])
+
